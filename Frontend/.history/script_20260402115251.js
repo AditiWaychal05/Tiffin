2140082ca6ac2675ee15c40d05c09d49e2275c7c
Shreka <shrekapoojari6@gmail.com>
2026-04-02 12:28:47 +0530

    Added frontend code

---
+++ Frontend/.history/script_20260402115251.js
@@ -0,0 +1,9 @@
+function loginUser(event) {
+    event.preventDefault();
+    alert("Login Successful (Demo Only)");
+}
+
+function submitFeedback(event) {
+    event.preventDefault();
+    alert("Feedback Submitted!");
+}
